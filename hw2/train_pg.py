dd3950783134def793c77f973a7d9c48de36b674
David Tao <t-rutao@microsoft.com>
2018-05-24 17:44:17 -0400

    first

--- hw2/train_pg.py
+++ hw2/train_pg.py
@@ -34,8 +34,18 @@ def build_mlp(
     #========================================================================================#
 
     with tf.variable_scope(scope):
-        # YOUR_CODE_HERE
-        pass
+        dense = input_placeholder
+        for _ in range(n_layers):
+            dense = tf.layers.dense(
+                input=dense,
+                units=size,
+                activation=activation
+            )
+    return tf.layers.dense(
+        inputs=dense,
+        units=output_size,
+        activation=output_activation
+    )
 
 def pathlength(path):
     return len(path["reward"])
